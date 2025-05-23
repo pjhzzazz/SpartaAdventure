990f3cf4cdc130577c7a1f4b86f1d2f0ce1da88f
pjhzzazz <pjhzzazzz@naver.com>
2025-05-23 16:50:17 +0900

    edit final

--- Assets/Scripts/Bridge/MovingPlatform.cs
+++ Assets/Scripts/Bridge/MovingPlatform.cs
@@ -31,6 +31,7 @@ public class MovingPlatform : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
             if (Vector3.Distance(transform.position, targetPos) < 0.01f)
             {
+                transform.rotation *= Quaternion.Euler(0, 180, 0);
                 go = false;
                 back = true;
             }
@@ -41,6 +42,7 @@ public class MovingPlatform : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, startPos, Time.deltaTime * speed);
             if (Vector3.Distance(transform.position, startPos) < 0.01f)
             {
+                transform.rotation *= Quaternion.Euler(0, 180, 0);
                 go = true;
                 back = false;
             }
